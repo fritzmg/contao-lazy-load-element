0ad8a4353a8e4bd87952e5d5a8545ce1257420cc
fritzmg <email@spikx.net>
2017-01-16 12:27:49 +0100

    fix theme template folder not being considered (#1)

--- system/modules/lazy_load_element/classes/LazyLoadInterface.php
+++ system/modules/lazy_load_element/classes/LazyLoadInterface.php
@@ -26,12 +26,63 @@ class LazyLoadInterface
 		{
 			$objPage = \PageModel::findWithDetails( $pageId );
 		}
-		
-		// set the language, if page object is available
-		$GLOBALS['TL_LANGUAGE'] = ( null !== $objPage ) ? $objPage->language : $GLOBALS['TL_LANGUAGE'];
 
+		// check for page object
+		if( null !== $objPage )
+		{
+			// set the language
+			$GLOBALS['TL_LANGUAGE'] = $objPage->language;
+
+			// Get the page layout
+			$blnMobile = ($objPage->mobileLayout && \Environment::get('agent')->mobile);
+
+			// Override the autodetected value
+			if (\Input::cookie('TL_VIEW') == 'mobile')
+			{
+				$blnMobile = true;
+			}
+			elseif (\Input::cookie('TL_VIEW') == 'desktop')
+			{
+				$blnMobile = false;
+			}
+
+			$intId = ($blnMobile && $objPage->mobileLayout) ? $objPage->mobileLayout : $objPage->layout;
+			$objLayout = \LayoutModel::findByPk($intId);
+
+			// check for layout
+			if( null !== $objLayout )
+			{
+				$objPage->hasJQuery = $objLayout->addJQuery;
+				$objPage->hasMooTools = $objLayout->addMooTools;
+				$objPage->isMobile = $blnMobile;
+
+				// HOOK: modify the page or layout object
+				if (isset($GLOBALS['TL_HOOKS']['getPageLayout']) && is_array($GLOBALS['TL_HOOKS']['getPageLayout']))
+				{
+					foreach ($GLOBALS['TL_HOOKS']['getPageLayout'] as $callback)
+					{
+						\System::importStatic($callback[0])->{$callback[1]}($objPage, $objLayout, $this);
+					}
+				}
+
+				/** @var \ThemeModel $objTheme */
+				$objTheme = $objLayout->getRelated('pid');
+
+				// Set the layout template and template group
+				$objPage->template = $objLayout->template ?: 'fe_page';
+				$objPage->templateGroup = $objTheme->templates;
+
+				// Store the output format
+				list($strFormat, $strVariant) = explode('_', $objLayout->doctype);
+				$objPage->outputFormat = $strFormat;
+				$objPage->outputVariant = $strVariant;
+			}
+		}
+
+		// get the type and element ID
 		list( $strType, $intId ) = trimsplit('::', \Input::get('element'));
 
+		// prepare element
 		$objElement = null;
 
 		// determine the type
